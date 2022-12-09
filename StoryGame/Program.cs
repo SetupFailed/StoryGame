7eb87f9135f9d60383205cbe975d1c083fd1d070
SetupFailed <thomas.karlsson619@gmail.com>
2022-12-09 21:53:27 +0100

    coppied the code and changed name

---
+++ StoryGame/Program.cs
@@ -0,0 +1,43 @@
+﻿using Raylib_cs;
+
+int widthX = 1600;
+int heightY = 900;
+
+
+
+void choice1(){
+  Raylib.DrawCircle(450,heightY-200,75,Color.WHITE);
+}
+
+void choice2(){
+  Raylib.DrawCircle(widthX-450, heightY-200,75,Color.WHITE);
+}
+
+void character(){
+  Raylib.DrawCircle(widthX/2,heightY-200,30,Color.WHITE);
+}
+
+
+
+Raylib.InitWindow(widthX, heightY, "The title of my window");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+  Raylib.BeginDrawing();
+  
+  Raylib.ClearBackground(Color.BLACK);
+  
+  character();
+
+  choice1();
+  choice2();
+
+  if(Raylib.IsKeyPressed(KeyboardKey.KEY_A) )
+  {
+    
+  }
+  
+  
+  Raylib.EndDrawing();
+}
